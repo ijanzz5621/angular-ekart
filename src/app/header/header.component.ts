4d9b24f646c156323af48040b8098ddb4d0b9101
Sharizan Redzuan <sharizan_81@yahoo.com>
2024-05-16 22:01:50 +0800

    Add header details

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -3,9 +3,10 @@ import { Component } from "@angular/core";
 @Component({
     standalone: true,
     selector: "app-header",
-    template: `
-        <h3>This is a header</h3>
-    `
+    // template: `
+    //     <h3>This is a header</h3>
+    // `
+    templateUrl: "./header.component.html"
 })
 
 export class HeaderComponent {
