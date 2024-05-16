65f79b0dba0e93d3c4b8b8012beedcc23130d554
Sharizan Mohd Redzuan <SRedzuan@slb.com>
2024-05-16 18:14:19 +0800

    Add and modify components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  providers: []
 })
 export class AppComponent {
   title = 'e-Kart';
---
+++ src/app/header/header.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+
+@Component({
+    standalone: true,
+    selector: "app-header",
+    template: `
+        <h3>This is a header</h3>
+    `
+})
+
+export class HeaderComponent {
+
+}
