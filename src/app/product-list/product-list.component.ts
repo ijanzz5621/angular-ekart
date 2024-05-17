58fb90a6e4fa068480cc5dbfeb39bd59fd24f2ce
Sharizan Redzuan <sharizan_81@yahoo.com>
2024-05-17 20:49:34 +0800

    one way data binding lesson

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -15,17 +15,37 @@ export class ProductListComponent {
 
   name: string = "Sharizan Redzuan";
 
+  addToCart: number = 0;
+
   product = {
     name: "iPhone 14",
     price: 1999,
     color: "Black",
     discountedPrice: 799,
     discount: 8.5,
-    inStock: 55,
+    inStock: 5,
     pImage: '/assets/images/iphone.png'
   }
 
   getDiscountedPrice() {
     return this.product.price - (this.product.price * this.product.discount / 100);
   }
+
+  onNameChanged(event: any) {
+    console.log(event)
+    this.name = event.target.value;
+  }
+
+  addItemToCart() {
+    this.addToCart += 1;
+  }
+
+  decrementCartValue() {
+    this.addToCart--;
+  }
+
+  incrementCartValue() {
+    this.addToCart++;
+  }
+
 }
